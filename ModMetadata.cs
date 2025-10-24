471285371b7156e37d9209bea8661bf1f7a960f3
Szonszczyk <Szonszczyk@users.noreply.github.com>
2025-10-24 06:11:05 +0200

    Added url

--- ModMetadata.cs
+++ ModMetadata.cs
@@ -12,7 +12,7 @@ namespace CaliberSplitAmmoCases
         public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
         public override List<string>? Incompatibilities { get; init; } = [];
         public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = [];
-        public override string? Url { get; init; } = ""; //TODO
+        public override string? Url { get; init; } = "https://github.com/Szonszczyk/CaliberSplitAmmoCases-csharp/"; //TODO
         public override bool? IsBundleMod { get; init; } = false;
         public override string? License { get; init; } = "MIT";
     }
